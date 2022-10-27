fa68e1f47b9033b5a4b284c146c4f2047ac57884
Prime010 <116540734+Prime010@users.noreply.github.com>
2022-10-27 11:47:56 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY5_ALVAREZ/ACTIVITY_05/ACTIVITY_05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Activity5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = givenlist(new List<int>(new int[] { 10, 20, 35, 65, 53, 48, 5, 1 }));
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        public static List<int> givenlist(List<int> nums)
+        {
+            Console.WriteLine("Numbers Input: 10, 20, 35, 65, 53, 48, 5, 1");
+            Console.WriteLine("Output:");
+            return nums.Where(n => n % 10 < 5).ToList();
+        }
+    }
+}
