49846f57a93e399ecc7929da2046806c64ad3e56
Prime010 <116540734+Prime010@users.noreply.github.com>
2022-10-27 10:15:36 +0800

    Add files via upload

---
+++ ITEC104_ACTIVITY3_ALVAREZ/ACTIVITY_3/ACTIVITY_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Activity_03
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] emails = { "yuriprime@gmail.com", "primezeravla@gmail.com", "alvarezjulian@gmail.com" };
+            string[] usernames = { "yuriprime_5", "primezeravla_5", "alvarezjulian_5" };
+            string[] passwords = { "PrimeAlvarez1", "PRimeAlvarez2", "PrimeAlvatrez3" };
+
+            Console.Write("Index: ");
+            int index = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nemail: {0}", emails[index]);
+            Console.WriteLine("username: " + usernames[index]);
+            Console.WriteLine("password: " + passwords[index]);
+
+            Console.ReadKey();
+            Console.WriteLine("\nPress any key to Continue. . .");
+        }
+    }
+}
